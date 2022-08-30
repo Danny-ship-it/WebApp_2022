d75e6c65539ce46087797f0f7a3873f0ef8e6650
Danny <denniso@hiof.no>
2022-08-30 20:28:26 +0200

    Oppgave 2 er ferdig

--- Oblig_1_Intro_JavaScript/oppgave2/index.js
+++ Oblig_1_Intro_JavaScript/oppgave2/index.js
@@ -12,8 +12,10 @@ const users = [
   { id: 10, name: "Morgan", age: 87 }
 ];
 
+// TODO: Hent HTML #id med getElementById
 const userUl = document.getElementById("users");
 
+// TODO: Lag en funksjon som kan brukes til å skrive ut HTMLen for å se brukerene. Du kan bruke users importert over for å hente en liste med 10 brukere
 const createTableUI = (users) => {
   userUl.innerHTML = null;
   userUl.innerHTML += `<li><span><span>Id</span>Navn</span><span>Alder</span></li>`;
@@ -22,13 +24,29 @@ const createTableUI = (users) => {
   }
 };
 
-// TODO: Hent HTML #id med getElementById
-// TODO: Lag en funksjon som kan brukes til å skrive ut HTMLen for å se brukerene. Du kan bruke users importert over for å hente en liste med 10 brukere
 // TODO: Lag en funksjon som håndterer søket og oppdaterer grensesnittet med resultatet fra søket
-// TODO: Lag en funksjon som håndterer filteret og oppdaterer grensesnittet med resultatet fra filteret
-// TODO: Lytt til tastatur klikk på søkefeltet, den skal trigge søkefunksjonen (handleSearch)
-// TODO: Lytt til klikk på filter-knappen, den skal trigge filterfunksjonen (handleFilter)
 
+
+// TODO: Lag en funksjon som håndterer filteret og oppdaterer grensesnittet med resultatet fra filteret
+function search(input){
+  const result = users.filter(user => user.name.toLowerCase().startsWith(input.toLowerCase()));
+  createTableUI(result);
+}
+
+function filter(input){
+  const result = users.filter(user => (user.age >= parseInt(input)) );
+  createTableUI(result);
+}
+// TODO: Lytt til tastatur klikk på søkefeltet, den skal trigge søkefunksjonen (handleSearch)
+const searchInput = document.getElementById("name");
+searchInput.addEventListener("input", function(){search(document.getElementById("name").value)});
+
+// TODO: Lytt til klikk på filter-knappen, den skal trigge filterfunksjonen (handleFilter)
+const filterBtn = document.getElementById("filter");
+filterBtn.addEventListener("click", function(){filter(document.getElementById("age").value)});
+
+
+//TODO main
 const main = () => {
   createTableUI(users);
 };
