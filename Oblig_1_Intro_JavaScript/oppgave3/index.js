06fe408afef39c39874cfb42652c793ff540cdc5
Danny <denniso@hiof.no>
2022-08-31 22:58:30 +0200

    Started 3 and 4

--- Oblig_1_Intro_JavaScript/oppgave3/index.js
+++ Oblig_1_Intro_JavaScript/oppgave3/index.js
@@ -1,19 +1,69 @@
 import "./styles.css";
 
-// TODO: Sett startverdien for de ulike tellerene du trenger for å ha kontroll på
-// - antall feil
-// - posisjonen til bokstaven du skal skrive
-// - ordet som skal vises
+// TODO: Sett startverdien for de ulike tellerene
+let wordCounter;
+let positionCounter;
+let wrongCounter;
+
 // TODO: Lag en liste med ulike ord
+const words = ["Apple", "Horse", "Man", "Monkey", "Manki", "Car", "Bystander", "Mureder", "Fun", "Fanta", "Coca", "Caramba", "Camel", "Mr", "Mrs", "Lisa", "Garden", "Camper", "Reatrded", "Sniper", "Rifle", "Greece", "Assassin", "Creed"];
 // TODO: Hent ut HTML #ider og knappen
-// TODO: Lag en funksjon for å skrive ut ordet som brukeren skal skrive eller en medling om at det ikke er flere ord igjen
-// TODO: Lag en funksjon som gjør at vi kan bytte ord. Må også oppdatere tellerene
-// TODO: Lag en funksjon for å sjekke om vi har skrevet riktig bokstav. Må ta hensyn til plassen i ordet vi skal skrive
-// TODO: Lag en funksjon for å sjekke om posisjonen vi er på er lik lengden på ordet vi skal skrive. Det betyr at vi er ferdig med ordet og kan bytte ord
-// TODO: Lag en funksjon som trigges når vi skriver på tastaturen og basert på hva vi skriver / posisjonen vi er på gjør nødvendige oppdateringer av grensesnittet
-// TODO: Lag en funksjon som kan brukes til å oppdatere grensesnittet
-// - Vise antall feil
-// - Vise ordet vi skal skrive
-// - Vise hvor langt vi har kommet (posisjonen) på det ordet vi skal skrive
-// TODO: Lytt til keyup på window
-// TODO: Lytt til klikk på knappen. Klikket skal gjøre knappen "disabled" samt starte oppgaven
+
+// TODO: Hent ut HTML #id og knappen
+const word = document.getElementById("word");;
+const wrongs = document.getElementById("wrongs");
+const letter = document.getElementById("letter");
+const button = document.getElementsByTagName('button')[0];
+
+
+const setWord = () => {
+  // TODO: Skriv ut ordet som brukeren skal skrive eller en medling om at det ikke er flere ord igjen
+  word.innerHTML = "";
+};
+
+const changeWord = () => {
+  // TODO: Nullstill posisjonstelleren
+  // TODO: Oppdater telleren vi bruker for å velge ut ord
+  setWord();
+};
+
+// TODO: Sjekk vi har skrevet riktig bokstav. Må ta hensyn til plassen i ordet vi skal skrive
+const checkPosition = (word, position, letter) => {
+  return "";
+};
+
+// TODO: Sjekk om posisjonen vi er på er lik lengden på ordet vi skal skrive. Det betyr at vi er ferdig med ordet og kan bytte ord
+const wordIsCorrect = (word, position) => {
+  return "";
+};
+
+const handleKeyUp = ({ key }) => {
+  // TODO: Hent ut ordet vi skal skrive
+  const word = "";
+  if (checkPosition(word, positionCounter, key)) {
+    // TODO: Øk posisjonstelleren
+    if (wordIsCorrect(word, positionCounter)) {
+      // TODO: Trigg funksjonen som bytter ord
+    }
+  } else {
+    // TODO: Oppdater telleren for "feil"
+  }
+  updateUI(key);
+};
+
+const updateUI = (key) => {
+  if (words[wordCounter]) {
+    wrongs.innerHTML = wrongCounter;
+    letter.innerHTML = key;
+    word.innerHTML = `<span class="green">${words[wordCounter].slice(
+      0,
+      positionCounter
+    )}</span>${words[wordCounter].slice(positionCounter)}`;
+  }
+};
+
+window.addEventListener("keyup", handleKeyUp);
+button.addEventListener("click", () => {
+  button.disabled = true;
+  setWord();
+});
